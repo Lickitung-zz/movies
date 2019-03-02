6bf0e4511b51b01c2fb156c287437eb988f2ea11
“SIeep” <Brandon_n_evans@live.com>
2019-03-01 19:38:00 -0600

    add CRUD and RESTful to movies app

--- movies/app/controllers/api/movies_controller.rb
+++ movies/app/controllers/api/movies_controller.rb
@@ -1,2 +1,34 @@
 class Api::MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+    render "index.json.jbuilder"
+  end
+
+  def create
+    @movie = Movie.new(
+      title: params[:title],
+      runtime: params[:runtime]
+      )
+    @movie.save
+    render "show.json.jbuilder"
+  end
+
+  def show
+    @movie = Movie.find_by(id: params[:id])
+    render "show.json.jbuilder"
+  end
+
+  def update
+    @movie = Movie.find_by(id: params[:id])
+    @movie.title = params[:title] || @movie.title
+    @movie.runtime = params[:runtime] || @movie.runtime
+    @movie.save
+    render "show.json.jbuilder"
+  end
+
+  def destroy
+    movie = Movie.find_by(id: params[:id])
+    movie.destroy
+    render json: {message: "Movie successfully destroyed"}
+  end
 end
